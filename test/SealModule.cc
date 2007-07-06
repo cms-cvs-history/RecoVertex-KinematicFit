449df37e3d34d27a7c6b0d8a1e6c58a652f51bfa
cmsbuild <>
2007-07-06 14:08:17 +0000

    This commit was manufactured by cvs2git to create tag 'RecoVertex- KinematicFit-CMSSW_1_7_6'.
    Sprout from master 2007-07-06 14:07:27 UTC Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch> 'Moved from test directory'
    Delete:
        test/BuildFile
        test/KineExample.cc
        test/KineExample.h
        test/SealModule.cc

--- test/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoVertex/KinematicFit/test/KineExample.h"
-
-DEFINE_SEAL_MODULE();
-
-DEFINE_ANOTHER_FWK_MODULE(KineExample);
